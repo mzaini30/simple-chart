18c6f240f521e37142b443a11bb7e69102066293
Zen <muhzaini30@gmail.com>
2019-10-14 05:28:36 +0800

    Fixed chart if all values are 0

--- simple-chart.js
+++ simple-chart.js
@@ -3,7 +3,6 @@ simple_chart = function(div, data){
 	// deklarasi parameter
 	height = 100
 
-	// $(div).html('tes')
 	$(div).css({
 		width: '100%',
 		height: height,
@@ -14,30 +13,26 @@ simple_chart = function(div, data){
 	for (n in data.data){
 		apakah_total_data_nol += Number(data.data[n])
 	}
-	if (apakah_total_data_nol == 0){
-		$(div).css({
-			display: 'none'
-		})
-	}
 
-	banyak_data = data.data.length // 6
-	lebar_elemen = $(div).width() / banyak_data // 48 (px)
-	// jumlah_data = 0
+	banyak_data = data.data.length
+	lebar_elemen = $(div).width() / banyak_data
 	nilai_tertinggi = 0
 	for (n in data.data){
 		if (data.data[n] == null){
 			data.data[n] = 0
 		}
-		// jumlah_data += Number(data.data[n]) // 10
 		if (Number(data.data[n]) >= nilai_tertinggi){
 			nilai_tertinggi = data.data[n]
 		}
 	}
-	// $('title').html(nilai_tertinggi)
 	$(div).html('')
 	for (n in data.data){
-		// width = lebar_elemen
-		height_elemen = Number(data.data[n]) / nilai_tertinggi * height // harusnya dibagi sama nilai tertinggi
+		if (apakah_total_data_nol == 0){
+			height_elemen = 1
+		} else {
+			height_elemen = Number(data.data[n]) / nilai_tertinggi * height
+		}
+		
 		height_kosong = height - height_elemen
 		$(div).append('<div style="float: left; width: ' + lebar_elemen + 'px; height: ' + height + 'px;"><div style="height: ' + height_kosong + 'px;"></div><div style="height: ' + height_elemen + 'px; background: ' + data.background[n] + '; border: 1px solid '+ data.color[n] + ';"></div></div>')
 	}
